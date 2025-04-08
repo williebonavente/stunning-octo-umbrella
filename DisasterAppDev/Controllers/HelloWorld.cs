38dfce620f8c2aa92ba99c4c07c806279b8aba31
Willie M. Bonavente <76456752+williebonavente@users.noreply.github.com>
2025-04-08 22:49:15 +0800

    Add HelloWorldController

---
+++ DisasterAppDev/Controllers/HelloWorld.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DisasterAppDev.Controllers
+{
+    public class HelloWorld : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Hello, World");
+        }
+    }
+}
