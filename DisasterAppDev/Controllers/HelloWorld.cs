3adc33b5f536ee69bc60022aae3231ba7ff917ce
Willie M. Bonavente <76456752+williebonavente@users.noreply.github.com>
2025-04-14 23:10:25 +0800

    Creating Collections

--- DisasterAppDev/Controllers/HelloWorld.cs
+++ DisasterAppDev/Controllers/HelloWorld.cs
@@ -7,12 +7,13 @@ namespace DisasterAppDev.Controllers
     {
         public IActionResult Index()
         {
-            var model = new TestModel
-            {
-                Id = 1,
-                Name = "Test Item",
-                Description = "This is a test description."
-            };
+            var model = new List<TestModel>
+           {
+               new TestModel { Id = 1, Name = "Test Item 1", Age  = 25, Description = "Description 1" },
+               new TestModel { Id = 2, Name = "Test Item 2", Age = 30, Description = "Description 2" },
+               new TestModel { Id = 3, Name = "Test Item 3", Age = 39, Description = "Description 3" }
+           };
+
             return View(model); // Pass the model  to the view
         }
     }
--- DisasterAppDev/Models/TestModel.cs
+++ DisasterAppDev/Models/TestModel.cs
@@ -4,6 +4,9 @@
     {
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
+        public int Age { get; set; }
         public string Description { get; set; } = string.Empty;
+        
+
     }
 }
