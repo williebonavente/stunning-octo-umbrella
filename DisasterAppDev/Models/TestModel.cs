21a2c40ba6102d3b57b12e31e941cb7eab0d056e
Willie M. Bonavente <76456752+williebonavente@users.noreply.github.com>
2025-04-18 23:42:39 +0800

    Ecommerce init

--- DisasterAppDev/Models/TestModel.cs
+++ DisasterAppDev/Models/TestModel.cs
@@ -1,12 +1,18 @@
-﻿namespace DisasterAppDev.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DisasterAppDev.Models
 {
     public class TestModel
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Name is required.")]
+        [StringLength(50, ErrorMessage = "Name cannot exceed 50 characters.")]
         public string Name { get; set; } = string.Empty;
+        [Range(18,  100, ErrorMessage = "Age must be between 18 and 100." )]
         public int Age { get; set; }
+        [Required(ErrorMessage = "Description in required.")]
         public string Description { get; set; } = string.Empty;
-        
+
 
     }
 }
